08274c0e91d3d00cb314906a37849c6ae7135852
HLLLG <2162803956@qq.com>
2026-05-10 17:35:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-calendar v-model:value="value" @panelChange="onPanelChange" />
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Dayjs } from 'dayjs'
+const value = ref<Dayjs>()
+const onPanelChange = (value: Dayjs, mode: string) => {
+  console.log(value, mode)
+}
+</script>
+
+<style scoped></style>
